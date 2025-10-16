7dfec97a74a021a44a8e52fdd530e591fd0095f8
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-10-16 17:00:54 +0900

    feat(calculator): 기본 구분자 확인
    입력값이 기본 구분자로 구분되는지 확인하고, split을 하여 숫자들을 나누었습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,17 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
+
+		// 기본 구분자 확인 (입력값의 처음 시작값이 //로 시작하지 않으면 기본 구분자 or 잘못된 값)
+		if(!input.startsWith("//")) {
+			String[] nums = input.split(",:");
+		}
+
     }
 }
