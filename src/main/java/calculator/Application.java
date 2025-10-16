e9f1133be6657f75c23f29722c7da4145b0456a4
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-10-16 17:59:32 +0900

    feat(calculator): 예외 처리
    try-catch 구문으로 잘못된 값을 입력시 illegalArgumentException을 throw하도록

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -14,24 +14,28 @@ public class Application {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.next();
 
-		// 기본 구분자 확인 (입력값의 처음 시작값이 //로 시작하지 않으면 기본 구분자 or 잘못된 값)
-		if(!input.startsWith("//")) {
-			String[] nums = input.split("[,:]");
-			int sum = 0;
-			for (String num : nums) {
-				sum += Integer.parseInt(num);
-			}
+		try {
+			// 기본 구분자 확인 (입력값의 처음 시작값이 //로 시작하지 않으면 기본 구분자 or 잘못된 값)
+			if (!input.startsWith("//")) {
+				String[] nums = input.split("[,:]");
+				int sum = 0;
+				for (String num : nums) {
+					sum += Integer.parseInt(num);
+				}
 
-			System.out.println(sum);
-		} else {
-			Map<String, String> map = getCustomSeperator(input);
-			String[] nums = map.get("input").split(map.get("seperator"));
-			int sum = 0;
-			for (String num : nums) {
-				sum += Integer.parseInt(num);
-			}
+				System.out.println(sum);
+			} else {
+				Map<String, String> map = getCustomSeperator(input);
+				String[] nums = map.get("input").split(map.get("seperator"));
+				int sum = 0;
+				for (String num : nums) {
+					sum += Integer.parseInt(num);
+				}
 
-			System.out.println(sum);
+				System.out.println(sum);
+			}
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
 		}
     }
 
