19064039a1525c2d8bf7dfce2b7f5706593802be
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-10-16 17:53:29 +0900

    feat(calculator): 커스텀 구분자 확인후 값 반환
    입력값이 커스텀 구분자인 입력값들을 StringTokenizer로 나눈후 Map으로 반환하는 함수를 생성하였습니다.
    그 이후 더하여 반환하는 코드를 추가하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,8 +16,30 @@ public class Application {
 
 		// 기본 구분자 확인 (입력값의 처음 시작값이 //로 시작하지 않으면 기본 구분자 or 잘못된 값)
 		if(!input.startsWith("//")) {
-			String[] nums = input.split(",:");
-		}
+			String[] nums = input.split("[,:]");
+			int sum = 0;
+			for (String num : nums) {
+				sum += Integer.parseInt(num);
+			}
 
+			System.out.println(sum);
+		} else {
+			Map<String, String> map = getCustomSeperator(input);
+			String[] nums = map.get("input").split(map.get("seperator"));
+			int sum = 0;
+			for (String num : nums) {
+				sum += Integer.parseInt(num);
+			}
+
+			System.out.println(sum);
+		}
     }
+
+	public static Map<String, String> getCustomSeperator(String input) {
+		Map<String, String> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(input, "\\n");
+		map.put("seperator",st.nextToken().replaceAll("//", ""));
+		map.put("input", st.nextToken());
+		return map;
+	}
 }
